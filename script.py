912c0680079c8db2712907496869c5f0f4fe235b
Fergus McClean <fergus.mcclean@ncl.ac.uk>
2020-11-18 13:46:40 +0000

    add real sample data and make script.py executable

--- script.py
+++ script.py
@@ -1,11 +1,12 @@
 import time
 import os
 import requests
+from glob import glob
 
 
 def upload_periodically(infinite=True):
     interval = os.getenv('INTERVAL', 15)
-    path = os.getenv('DATA_PATH', 'tests/data.csv')
+    path = glob(os.path.join(os.getenv('DATA_PATH', 'tests/data'), '*Lysimeter*'))[0]
     url = os.getenv('UPLOAD_URL', 'test')
     while True:
         with open(path, 'rb') as f:
@@ -13,3 +14,7 @@ def upload_periodically(infinite=True):
         if not infinite:
             break
         time.sleep(interval*60)
+
+
+if __name__ == '__main__':
+    upload_periodically()
