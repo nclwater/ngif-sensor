97a01e90c31d9f467b98e0ddbe8577ad9cabf797
Fergus McClean <fergus.mcclean@ncl.ac.uk>
2020-11-19 20:35:55 +0000

    Add Dockerfile

--- script.py
+++ script.py
@@ -1,19 +1,19 @@
 import time
 import os
 import requests
-import subprocess
 from glob import glob
+from datetime import datetime
 
 interval = os.getenv('INTERVAL', 15)
 path = os.getenv('DATA_PATH', 'tests/data')
 url = os.getenv('UPLOAD_URL', 'test')
-sensor_ports = os.getenv('SENSOR_PORTS', '').split()
 
 
 def upload_periodically(infinite=True):
 
     while True:
         for file_path in glob(os.path.join(path, '*(*')):
+            print(f'{datetime.now():%Y-%m-%D %H:%M:%S} Uploading {file_path}')
             with open(file_path, 'rb') as f:
                 requests.post(url, files={'upload_file': f})
         if not infinite:
